f782abdf00babe668b746225fd80c40a03d53f03
Denilson Gama <denilson.gama@gamadeveloper.com.br>
2018-07-26 22:38:24 -0300

    Second Commit - Criação de botões e caixa de texto

--- app/src/main/java/br/com/fiap/helloword/MainActivity.kt
+++ app/src/main/java/br/com/fiap/helloword/MainActivity.kt
@@ -2,11 +2,23 @@ package br.com.fiap.helloword
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import br.com.fiap.helloword.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Criando listener onClick
+        btn_CliqueAqui.setOnClickListener {
+
+            //Recuperando o texto digitado pelo usuário e atribuindo a uma String
+            var nome:String = edt_Nome.editableText.toString()
+
+            //Alterando o texto dentro do TextView
+            txv_Resultado.text = nome
+        }
     }
 }
