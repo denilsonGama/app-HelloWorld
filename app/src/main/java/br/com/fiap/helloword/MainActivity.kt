fff57cf30ffa65868cd6312e4a8f5937fc7387e3
Denilson Gama <denilson.gama@gamadeveloper.com.br>
2018-07-27 20:19:31 -0300

    Second commit - 2ª tela

--- app/src/main/java/br/com/fiap/helloword/MainActivity.kt
+++ app/src/main/java/br/com/fiap/helloword/MainActivity.kt
@@ -1,8 +1,9 @@
 package br.com.fiap.helloword
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import br.com.fiap.helloword.R
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,14 +12,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var txv_Resultado = findViewById<TextView>(R.id.txv_Resultado)
+
         //Criando listener onClick
         btn_CliqueAqui.setOnClickListener {
 
             //Recuperando o texto digitado pelo usuário e atribuindo a uma String
-            var nome:String = edt_Nome.editableText.toString()
+            var nome: String = edt_Nome.editableText.toString()
 
             //Alterando o texto dentro do TextView
             txv_Resultado.text = nome
+
+            val intent = Intent(this, Proxima::class.java)
+            startActivity(intent)
         }
     }
 }
